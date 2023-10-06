0c1eb39ac503931c9b6d99aaa6bc5967ae7d92b6
Andrea <andrea.iommi741@gmail.com>
2023-10-06 10:30:47 +0200

    review the report and adjust some operation in the project

--- ExtremeNN.py
+++ ExtremeNN.py
@@ -1,5 +1,4 @@
 from numpy import ndarray, eye
-from numpy.linalg import inv
 from numpy.random import rand
 
 from NumericalUtils import cholesky, backwardSub, forwardSub
@@ -18,20 +17,8 @@ class ENeuralN:
         self.w1 = rand(hidden, features)  # resevoir
         self.w2 = None  # readout
 
-        self.resevoir = lambda x: activation(self.w1 @ x.T).T
+        self.resevoir = lambda x: activation(self.w1 @ x)
         self.alfa_reg = alfa_reg  # L2 regularization
-
-    def fit_normal_equation(self, x: ndarray, y: ndarray):
-        """
-        Fit the neural network using the normal equation
-        Find min_w(wH-Y) indeed w = (H.T * H + alpha*I) * H.T * Y
-        :param x: Input dataset
-        :param y: Target
-        :return: None
-        """
-        h = self.resevoir(x)
-        self.w2 = (inv(h.T @ h + self.alfa_reg * eye(h.shape[1])) @ h.T @ y).T
-        return
 
     def fit_cholesky(self, x: ndarray, y: ndarray):
         """
@@ -45,13 +32,12 @@ class ENeuralN:
         # Perform the first (resevoir) layer
         h = self.resevoir(x)
         # (1) Apply the cholesky factorization
-        r = cholesky(h.T @ h + self.alfa_reg * eye(h.shape[1])).T
+        r = cholesky(h @ h.T + self.alfa_reg * eye(h.shape[0])).T
         # (2) back substitution (r upper triangular)
-        z = backwardSub(r, y.T @ h)
+        z = backwardSub(r, y @ h.T)
         # (3) forward substitution (r.T lower triangular)
         self.w2 = forwardSub(r.T, z)
         return
 
     def __call__(self, x: ndarray) -> ndarray:
-        hidden = self.resevoir(x)
-        return (self.w2 @ hidden.T).T
+        return self.w2 @ self.resevoir(x)
