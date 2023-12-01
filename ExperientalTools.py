755d868f2ff13446b9ea819f360cbd15a07fe0f2
Andrea <andrea.iommi741@gmail.com>
2023-12-01 18:00:47 +0100

    re-arrange function, grid-search for cholesky and fista

--- ExperientalTools.py
+++ ExperientalTools.py
@@ -1,9 +1,8 @@
 import sys
-import time
+from typing import Tuple
 
 import numpy as np
 import pandas as pd
-from pandas import DataFrame
 from sklearn.model_selection import train_test_split, ParameterGrid
 from tqdm import tqdm
 
@@ -11,31 +10,56 @@ from ExtremeNN import ENeuralN
 from Utils import MSE
 
 
-def GridSearch(train_path: str, test_path: str, configs: dict):
+def prepare_dataset(train_path: str, test_path: str = None):
     train_set = pd.read_csv(train_path, header=None, index_col=0)
-    test_set = pd.read_csv(test_path, header=None, index_col=0)
-
     # We have training set tr_x 1182 columns and 10 rows
     tr_x, tr_y = train_set.iloc[:, :10].to_numpy(), train_set.iloc[:, 10:].to_numpy()
-    ts_x, ts_y = test_set.iloc[:, :10].to_numpy().T, test_set.iloc[:, 10:].to_numpy().T
+
+    ts_x, ts_y = None, None
+    if test_path is not None:
+        test_set = pd.read_csv(test_path, header=None, index_col=0)
+        ts_x, ts_y = test_set.iloc[:, :10].to_numpy().T, test_set.iloc[:, 10:].to_numpy().T
 
     x_train, x_val, y_train, y_val = train_test_split(tr_x, tr_y, test_size=0.33)
     x_train, x_val = x_train.T, x_val.T
     y_train, y_val = y_train.T, y_val.T
 
+    return (x_train, y_train), (x_val, y_val), (ts_x, ts_y)
+
+
+def fit_cholesky(x_train: np.ndarray, y_train: np.ndarray, hidden: int,
+                 lambda_: float, activation=None, features_x: int = 10) -> ENeuralN:
+    model = ENeuralN(features_x, hidden, lambda_, activation)
+    model.fit_cholesky(x_train, y_train)
+    return model
+
+
+def fit_fista(x_train: np.ndarray, y_train: np.ndarray, hidden: int, lambda_: float,
+              max_inters: int = None, activation=None, features_x: int = 10) -> Tuple[ENeuralN, list[float]]:
+    model = ENeuralN(features_x, hidden, lambda_, activation)
+    mse_errors = model.fit_fista(x_train, y_train, max_inters)
+    return model, mse_errors
+
+
+def get_results(model: ENeuralN, x: np.ndarray, y: np.ndarray):
+    y_pred = model(x)
+    return MSE(y, y_pred)
+
+
+def GridSearch_cholesky(train_path: str, test_path: str = None, configs: dict = None):
+    (x_train, y_train), (x_val, y_val), (ts_x, ts_y) = prepare_dataset(train_path, test_path)
     min_error, best_model, best_conf = sys.maxsize, None, None
 
     progress = tqdm(ParameterGrid(configs))
     for single_conf in progress:
+
         hidden = single_conf["hidden"]
         lambda_ = single_conf["regularization"]
         activation = single_conf["activation_fun"]
 
-        model = ENeuralN(10, hidden, lambda_, activation)
-        model.fit_cholesky(x_train, y_train)
-        y_train_pred, y_val_pred = model(x_train), model(x_val)
-
-        mse_train, mse_val = MSE(y_train, y_train_pred), MSE(y_val, y_val_pred)
+        model = fit_cholesky(x_train, y_train, hidden, lambda_, activation)
+        mse_train = get_results(model, x_train, y_train)
+        mse_val = get_results(model, x_val, y_val)
 
         if mse_val < min_error:
             best_model = model
@@ -47,55 +71,40 @@ def GridSearch(train_path: str, test_path: str, configs: dict):
     print("\nThe best configuration is ", best_conf[0])
     print("Train error ", best_conf[1], " Validation error", best_conf[2])
 
-    y_test_pred = best_model(ts_x)
-    test_error = MSE(ts_y, y_test_pred)
-    print("Test error ", test_error)
+    if test_path is not None:
+        y_test_pred = best_model(ts_x)
+        test_error = MSE(ts_y, y_test_pred)
+        print("Test error ", test_error)
 
 
-def GridSearch_Time(train_path: str, configs: dict) -> DataFrame:
-    train_set = pd.read_csv(train_path, header=None, index_col=0)
+def GridSearch_fista(train_path: str, test_path: str = None, configs: dict = None):
 
-    # We have training set tr_x 1182 columns and 10 rows
-    tr_x, tr_y = train_set.iloc[:, :10].to_numpy(), train_set.iloc[:, 10:].to_numpy()
+    (x_train, y_train), (x_val, y_val), (ts_x, ts_y) = prepare_dataset(train_path, test_path)
+    min_error, best_model, best_conf = sys.maxsize, None, None
 
-    x_train, x_val, y_train, y_val = train_test_split(tr_x, tr_y, test_size=0.33)
-    x_train, x_val = x_train.T, x_val.T
+    progress = tqdm(ParameterGrid(configs))
+    for single_conf in progress:
 
-    all_configuration = []
-
-    for single_conf in tqdm(ParameterGrid(configs)):
         hidden = single_conf["hidden"]
         lambda_ = single_conf["regularization"]
         activation = single_conf["activation_fun"]
+        max_iter = single_conf["max_iter"]
 
-        model = ENeuralN(10, hidden, lambda_, activation)
+        model = fit_fista(x_train, y_train, hidden, lambda_,max_iter, activation)
+        mse_train = get_results(model[0], x_train, y_train)
+        mse_val = get_results(model[0], x_val, y_val)
 
-        start_train = time.time()
-        model.fit_cholesky(x_train, y_train)
-        end_train = time.time()
+        if mse_val < min_error:
+            best_model = model[0]
+            best_conf = (single_conf, mse_train, mse_val)
+            min_error = mse_val
 
-        start_inference = time.time()
-        y_train_pred, y_val_pred = model(x_train), model(x_val)
-        end_inference = time.time()
-
-        mse_train, mse_val = MSE(y_train, y_train_pred), MSE(y_val, y_val_pred)
-
-        current_conf = (mse_train, mse_val, single_conf,
-                        {"train_time": end_train - start_train,
-                         "inference_time": end_inference - start_inference})
-        all_configuration.append(current_conf)
-
-    rank = sorted(all_configuration, key=lambda conf: conf[1])
-    best_conf = rank[0]
+        progress.set_postfix(minMSE=min_error)
 
     print("\nThe best configuration is ", best_conf[0])
     print("Train error ", best_conf[1], " Validation error", best_conf[2])
 
-    time_matrix = np.zeros((len(all_configuration), 3))
-
-    for idx, config in enumerate(all_configuration):
-        time_matrix[idx, 0] = config[2]["hidden"]
-        time_matrix[idx, 1] = config[0]
-        time_matrix[idx, 2] = config[1]
-
-    return DataFrame(time_matrix, columns=["Hidden", "Train time", "Inference time"])
+    if test_path is not None:
+        y_test_pred = best_model(ts_x)
+        test_error = MSE(ts_y, y_test_pred)
+        print("Test error ", test_error)
--- ExtremeNN.py
+++ ExtremeNN.py
@@ -1,5 +1,3 @@
-import random
-
 import numpy as np
 from numpy import ndarray, eye
 from numpy.random import rand
@@ -7,24 +5,22 @@ from numpy.random import rand
 from NumericalUtils import cholesky, backwardSub, forwardSub
 from Utils import max_eigenvalue, MSE
 
-import matplotlib.pyplot as plt
-
 
 class ENeuralN:
 
-    def __init__(self, features: int, hidden: int, alfa_reg: float, activation):
+    def __init__(self, features: int, hidden: int, regularization: float, activation):
         """
         Implementation of Extreme Neural network
         :param features: Number of input features
         :param hidden: Hidden nodes
-        :param alfa_reg: L2 regularization alpha
+        :param regularization: L2 regularization alpha
         :param activation: activation function
         """
         self.w1 = rand(hidden, features)  # resevoir
         self.w2 = None  # readout
 
         self.resevoir = lambda x: activation(self.w1 @ x)
-        self.alfa_reg = alfa_reg  # L2 regularization
+        self.regularization = regularization  # L2 regularization
 
     def fit_cholesky(self, x: ndarray, y: ndarray):
         """
@@ -40,7 +36,7 @@ class ENeuralN:
         # Perform the first (resevoir) layer
         h = self.resevoir(x)
         # (1) Apply the cholesky factorization
-        r = cholesky(h @ h.T + self.alfa_reg * eye(h.shape[0])).T
+        r = cholesky(h @ h.T + self.regularization * eye(h.shape[0])).T
         # (2) back substitution (r upper triangular)
         z = backwardSub(r, y @ h.T)
         # (3) forward substitution (r.T lower triangular)
@@ -51,21 +47,24 @@ class ENeuralN:
     def calc_lambda(lambda_):
         return (1 + np.sqrt(1 + 4 * np.power(lambda_, 2))) / 2
 
-    def fit_fista(self, x: ndarray, target: ndarray, iter_: int):
+    def fit_fista(self, x: ndarray, y: ndarray, max_iter: int) -> list[float]:
         # Perform the first (resevoir) layer
         h = self.resevoir(x)
+        # Perform the lipschitz constant
         L = max_eigenvalue(h)
-        print(1/L)
-        self.w2 = rand(2, h.shape[0])
-        w2_old = self.w2.copy()
-        c = 0
-        lambda_k_1 = 0
 
-        learning = []
-        for k in range(iter_):
-            y_k = w2_old + c * (self.w2 - w2_old)
-            grad_yk = (y_k @ h - target) @ h.T
-            self.w2 = y_k - (1/L) * grad_yk - self.alfa_reg * self.w2
+        # Initialize with random matrix (random weight)
+        self.w2 = rand(2, h.shape[0])
+        # "Previous weight"
+        w2_old = self.w2.copy()
+
+        c, lambda_k_1, current_iter = 0, 0, 0
+        mse_errors = []
+
+        while current_iter < max_iter:
+            z_k = w2_old + c * (self.w2 - w2_old)
+            grad_zk = (z_k @ h - y) @ h.T
+            self.w2 = z_k - (1 / L) * grad_zk - self.regularization * self.w2
 
             lambda_k = self.calc_lambda(lambda_k_1)
             c = (lambda_k_1 - 1) / lambda_k
@@ -73,14 +72,21 @@ class ENeuralN:
             w2_old = self.w2.copy()
 
             y_pred = self.w2 @ h
+            mse_error = MSE(y, y_pred)
+            mse_errors.append(mse_error)
+            current_iter += 1
 
-            mse = MSE(target, y_pred)
-            learning.append(mse)
-            print(mse)
+        return mse_errors
 
-        plt.plot(learning)
-        plt.ylim([-0.5, 10])
-        """
+    def fit_standard_SDG(self, x: ndarray, y: ndarray, max_iter: int):
+        # TO DO
+        pass
+
+    def __call__(self, x: ndarray) -> ndarray:
+        return self.w2 @ self.resevoir(x)
+
+
+"""
         c0 = 1
         y_0 = self.w2 * c0 * (self.w2 - w2_old)
         grad_y_0 = (y_0 @ h - target) @ h.T
@@ -90,7 +96,4 @@ class ENeuralN:
         y_1 = self.w2 + c1 * (self.w2 - w2_old)
         grad_y_1 = (y_0 @ h - target) @ h.T
         self.w2 = y_1 - (1 / L) * grad_y_1
-        """
-
-    def __call__(self, x: ndarray) -> ndarray:
-        return self.w2 @ self.resevoir(x)
+"""
